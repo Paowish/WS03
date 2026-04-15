d1cfeaca5415dc147d8cfa33ebdaa16965721046
Juneeca <juneeca.a.l@gmail.com>
2026-04-15 10:55:53 +0800

    Initial Commit WS03 Project

---
+++ Public/index.php
@@ -0,0 +1,8 @@
+<?php
+// define('BASE_PATH', dirname(__DIR__));
+// require basePath('helpers.php');
+
+    require '../helpers.php';
+    require basePath('views/home.view.php');
+    loadView('home');   
+?>
